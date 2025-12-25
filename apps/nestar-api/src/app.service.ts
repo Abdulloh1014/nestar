4f39c4024dd4857322c2d0c3b700dff6ea3606bc
Abdulloh1014 <abdullohmansurbekov73@gmail.com>
2025-12-25 19:48:18 +0900

    feat: connect MongoDB database

--- apps/nestar-api/src/app.service.ts
+++ apps/nestar-api/src/app.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 
-@Injectable()
+@Injectable()             //@Injectable → “bu klassni boshqa joyda ishlatish mumkin” degani.
 export class AppService {
   getHello(): string {
     return 'Welcome to Nestar Rest API Server!';
