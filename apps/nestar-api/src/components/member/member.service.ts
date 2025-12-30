d294e3665bacdc9f00a828b5c67353dfcbb1103d
Abdulloh1014 <abdullohmansurbekov73@gmail.com>
2025-12-30 13:06:11 +0900

    feat: develop global GraphQL error handling system

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -22,8 +22,8 @@ export class MemberService {
             // Authentication via Token
             return result;
         } catch (err) {
-             console.log('Error, Service.model:', err);
-             throw new BadRequestException(err);
+             console.log('Error, Service.model:', err.message);
+             throw new BadRequestException(Message.USED_MEMBER_NICK_OR_PHONE);
         }
     }
 
