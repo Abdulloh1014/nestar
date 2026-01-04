ba99136a498db83595a934a93b0a4005b5f92a28
Abdulloh1014 <abdullohmansurbekov73@gmail.com>
2026-01-04 15:36:24 +0900

    feat: devleop getAgents GraphQL api

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -1,5 +1,10 @@
 import { ObjectId } from 'bson';
 
+
+
+export const availableAgentStors = ['createdAt', 'updatedAt', 'memberLikes', 'memberViews', 'memberRank']
+
+
 export const shapeIntoMongoObjectId = (target: any) => {
     return typeof target === "string" ? new ObjectId(target) : target;
 };
