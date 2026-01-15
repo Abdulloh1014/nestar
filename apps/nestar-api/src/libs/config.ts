52d7f72fd6358a3c6afa17bab6bb5abe136e44bf
Abdulloh1014 <abdullohmansurbekov73@gmail.com>
2026-01-15 17:32:08 +0900

    feat: create subscribe, unsubscribe, getMemberFollowings and getMemberFollowers GraphQL APIs

--- apps/nestar-api/src/libs/config.ts
+++ apps/nestar-api/src/libs/config.ts
@@ -40,3 +40,25 @@ export const lookupMember = {
 		as: 'memberData',          // 4)  memberData nomi bilan qaytaradi
 	},
 };
+
+
+export const lookupFollowingData = {
+	$lookup: {
+		from: 'members',		   
+		localField: 'followingId',	
+		foreignField: '_id',       
+		as: 'followingData',          
+	},
+};
+
+export const lookupFollowerData = {
+	$lookup: {
+		from: 'members',           
+		localField: 'followerId',	
+		foreignField: '_id',       
+		as: 'followerData',          
+	},
+};
+
+
+
