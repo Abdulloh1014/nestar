9ccfc00d23e21a129b56b280ce1f30859bb6368d
Abdulloh1014 <abdullohmansurbekov73@gmail.com>
2026-01-18 12:59:53 +0900

    fix: modify like interaction related logic

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -14,6 +14,7 @@ import { LikeInput } from '../../libs/dto/like/like.input';
 import { LikeGroup } from '../../libs/enums/like.enum';
 import { LikeService } from '../like/like.service';
 import { Follower, Following, MeFollowed } from '../../libs/dto/follow/follow';
+import { lookupAuthMemberLiked } from '../../libs/config';
 
 
 @Injectable()
@@ -138,17 +139,23 @@ export class MemberService {
         // 'i' bu katta kichik harfni farqlamaslik uchun
         console.log("match:", match);
 
-        const result = await this.memberModel.aggregate([
+        const result = await this.memberModel
+        .aggregate([
             {$match: match},
             {$sort: sort},
             {
                 $facet: {
-                    list: [{$skip: (input.page - 1) * input.limit}, {$limit: input.limit}],
-                    metaCounter: [{$count: 'total'}],
+                    list: [
+                        { $skip: (input.page - 1) * input.limit }, 
+                        { $limit: input.limit },
+                        lookupAuthMemberLiked(memberId),
+                        ],
+                         metaCounter: [{ $count: 'total' }],
                 },
             },
-        ]). exec();
-        if(!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
+        ]). 
+        exec();
+        if (!result.length) throw new InternalServerErrorException(Message.NO_DATA_FOUND);
        
        return result[0];
    }
